880bc8f50af153107795e6e1139e3922abf7b7ac
dawood96 <46974692+dawood96@users.noreply.github.com>
2020-08-15 17:17:34 -0400

    Updates

---
+++ precache-manifest.6c5cc64ee854ff79f1c1769a1d1b2ccd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "958aa146c5105509791ca2b304361ce8",
+    "url": "/todo-list/index.html"
+  },
+  {
+    "revision": "e7a39bc0e643fa48bd9a",
+    "url": "/todo-list/static/css/main.13fbfe1f.chunk.css"
+  },
+  {
+    "revision": "01d36f55c2f5d6cf8563",
+    "url": "/todo-list/static/js/2.f39368ab.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/todo-list/static/js/2.f39368ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7a39bc0e643fa48bd9a",
+    "url": "/todo-list/static/js/main.d32d7762.chunk.js"
+  },
+  {
+    "revision": "27e019a4eb32999bd0bf",
+    "url": "/todo-list/static/js/runtime-main.88bd720a.js"
+  }
+]);
